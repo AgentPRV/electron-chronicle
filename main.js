ae9152a1f744a3b6374ac49954f8c06e5b84f4f6
AgentPRV <prakalpvarshney2@gmail.com>
2024-03-31 00:33:23 +0530

    set title by ipc

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron/main')
+const path = require('node:path')
 
 const createWindow = () => {
     const win = new BrowserWindow({
       width: 800,
-      height: 600
+      height: 600,
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js')
+      }
+    })
+
+    ipcMain.on('set-title', (event, title) => {
+      const webContents = event.sender
+      const win = BrowserWindow.fromWebContents(webContents)
+      win.setTitle(title)
     })
   
     win.loadFile('index.html')
